4adf1d8571d887f8d4ae4d73cd5d65eb1dbe559c
yuval.keidar <keidaryu@gmail.com>
2021-06-17 18:44:57 +0300

    Update share-config.js

--- flow-typed/types/share-config.js
+++ flow-typed/types/share-config.js
@@ -3,8 +3,6 @@ declare type ShareConfig = {
   shareUrl: string,
   embedUrl: string,
   enableTimeOffset: boolean,
-  useParentUrl: boolean,
   useNative: boolean,
-  UiConfForSharing: string,
   socialNetworks: ShareSocialNetworks
 };
